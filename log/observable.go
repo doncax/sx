d70867eb0a5db0bf111381d9ded36c2c22bb04df
doncax <s4b0nk@tuta.io>
2025-07-24 03:41:14 +0700

    remove useless logs

--- log/observable.go
+++ log/observable.go
@@ -2,9 +2,12 @@ package log
 
 import (
 	"context"
+	"errors"
 	"io"
 	"os"
 	"time"
+
+	"github.com/sagernet/sing/common/baderror"
 
 	"github.com/sagernet/sing/common"
 	F "github.com/sagernet/sing/common/format"
@@ -115,30 +118,45 @@ func (l *observableLogger) Log(ctx context.Context, level Level, args []any) {
 		return
 	}
 	nowTime := time.Now()
-	if level <= l.level {
-		if l.needObservable {
-			message, messageSimple := l.formatter.FormatWithSimple(ctx, level, l.tag, F.ToString(args...), nowTime)
-			if level == LevelPanic {
-				panic(message)
-			}
-			l.writer.Write([]byte(message))
-			if level == LevelFatal {
-				os.Exit(1)
-			}
-			l.subscriber.Emit(Entry{level, messageSimple})
-		} else {
-			message := l.formatter.Format(ctx, level, l.tag, F.ToString(args...), nowTime)
-			if level == LevelPanic {
-				panic(message)
-			}
-			l.writer.Write([]byte(message))
-			if level == LevelFatal {
-				os.Exit(1)
-			}
+	msgStr := F.ToString(args...)
+	if baderror.Contains(errors.New(msgStr),
+			"i/o timeout",
+			"tls: protocol is shutdown",
+			"handle stream request: read request: EOF",
+			"ws closed: 1000",
+			"ws closed: 1005",
+			"keepalive timeout",
+			"connection timed out",
+			"read multiplex stream request: EOF",
+			"name error",
+			"connection refused",
+			"drop connections by rule",
+			"drop by rule",
+	) {
+			return
+	}
+	if l.needObservable {
+		message, messageSimple := l.formatter.FormatWithSimple(ctx, level, l.tag, msgStr, nowTime)
+		if level == LevelPanic {
+			panic(message)
+		}
+		l.writer.Write([]byte(message))
+		if level == LevelFatal {
+			os.Exit(1)
+		}
+		l.subscriber.Emit(Entry{level, messageSimple})
+	} else {
+		message := l.formatter.Format(ctx, level, l.tag, msgStr, nowTime)
+		if level == LevelPanic {
+			panic(message)
+		}
+		l.writer.Write([]byte(message))
+		if level == LevelFatal {
+			os.Exit(1)
 		}
 	}
 	if l.platformWriter != nil {
-		l.platformWriter.WriteMessage(level, l.platformFormatter.Format(ctx, level, l.tag, F.ToString(args...), nowTime))
+		l.platformWriter.WriteMessage(level, l.platformFormatter.Format(ctx, level, l.tag, msgStr, nowTime))
 	}
 }
 
