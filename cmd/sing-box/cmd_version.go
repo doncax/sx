8aaf56fa705c51f9992404b43f6752b114fd11ba
doncax <s4b0nk@tuta.io>
2025-08-12 00:50:00 +0700

    build

--- cmd/sing-box/cmd_version.go
+++ cmd/sing-box/cmd_version.go
@@ -1,40 +1,40 @@
 package main
-
+ 
 import (
 	"os"
 	"runtime"
 	"runtime/debug"
-
+ 
 	C "github.com/sagernet/sing-box/constant"
-
+ 
 	"github.com/spf13/cobra"
 )
-
+ 
 var commandVersion = &cobra.Command{
 	Use:   "version",
 	Short: "Print current version of sing-box",
 	Run:   printVersion,
 	Args:  cobra.NoArgs,
 }
-
+ 
 var nameOnly bool
-
+ 
 func init() {
 	commandVersion.Flags().BoolVarP(&nameOnly, "name", "n", false, "print version name only")
 	mainCommand.AddCommand(commandVersion)
 }
-
+ 
 func printVersion(cmd *cobra.Command, args []string) {
 	if nameOnly {
 		os.Stdout.WriteString(C.Version + "\n")
 		return
 	}
-	version := "sing-box version " + C.Version + "\n\n"
-	version += "Environment: " + runtime.Version() + " " + runtime.GOOS + "/" + runtime.GOARCH + "\n"
-
+	version := "sing-box version " + C.Version + "\n"
+	version += "environment: " + runtime.Version() + " " + runtime.GOOS + "/" + runtime.GOARCH + "\n"
+ 
 	var tags string
 	var revision string
-
+ 
 	debugInfo, loaded := debug.ReadBuildInfo()
 	if loaded {
 		for _, setting := range debugInfo.Settings {
@@ -46,19 +46,13 @@ func printVersion(cmd *cobra.Command, args []string) {
 			}
 		}
 	}
-
+ 
 	if tags != "" {
-		version += "Tags: " + tags + "\n"
+		version += "tags: " + tags + "\n"
 	}
 	if revision != "" {
-		version += "Revision: " + revision + "\n"
+		version += "revision: " + revision + "\n"
 	}
-
-	if C.CGO_ENABLED {
-		version += "CGO: enabled\n"
-	} else {
-		version += "CGO: disabled\n"
-	}
-
+ 
 	os.Stdout.WriteString(version)
 }
